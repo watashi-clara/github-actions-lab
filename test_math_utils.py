2b13157070b652425e8eb1db41b3054ab5818eda
claraa.lrnt <96233494+watashi-clara@users.noreply.github.com>
2026-01-15 14:54:58 +0100

    Ajout du code et des tests de base

---
+++ test_math_utils.py
@@ -0,0 +1,7 @@
+from math_utils import add, subtract
+def test_add():
+    assert add(2, 3) == 5
+    assert add(-1, 1) == 0
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 3) == -3
